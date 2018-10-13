3dbb5dbcc101e1f778979505bdf8b445f2c4f85e
Jenna Brandon <jlbcodes@gmail.com>
2018-10-13 01:53:40 -0600

    Registered serviceWorkers and Aria audit 94%

---
+++ js/register-sw.js
@@ -0,0 +1,7 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log("Service Worker has been registered successfully!");
+  }).catch((e) => {
+    console.log("Couldn't register service worker... \n", e);
+  });
+}
