9fdd6ec4b0716e135b26f9dba238c9947c8448d6
Jenna Brandon <jlbcodes@gmail.com>
2018-10-16 09:39:31 -0600

    Fix: service worker is now caching offline

--- sw.js
+++ sw.js
@@ -1,63 +1,55 @@
 // help from the following sources: Alexandro Perez, Ryan Waite, Matthew Cranford, Doug Brown
-const appName = "restaurant-reviews";
-const staticCacheName = appName + "-v1.0";
-const contentImgsCache = appName + "-images";
 
-var allCaches = [
-  staticCacheName,
-  contentImgsCache
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
 ];
 
-/** At Service Worker Install time, cache all static assets */
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '/', // this caches index.html
-        '/restaurant.html',
-        '/css/styles.css',
-        '/js/dbhelper.js',
-        '/js/secret.js',
-        '/js/main.js',
-        '/js/restaurant_info.js',
-        'js/register-sw.js',
-        'data/restaurants.json'
-      ]);
-    })
-  );
-});
-
-/** At Service Worker Activation, Delete previous caches, if any */
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith(appName) &&
-                 !allCaches.includes(cacheName);
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll(cacheFiles);
         })
-      );
-    })
-  );
+    );
 });
 
-/** Hijack fetch requests and respond accordingly */
-self.addEventListener('fetch', function(event) {
-  const requestUrl = new URL(event.request.url);
-
-  // only highjack request made to our app (not mapbox maps or leaflet, for example)
-  if (requestUrl.origin === location.origin) {
-
-    // Since requests made to restaurant.html have search params (like ?id=1), the url can't be used as the
-    // key to access the cache, so just respondWith restaurant.html if pathname startsWith '/restaurant.html'
-  }
-
-  // Default behavior: respond with cached elements, if any, falling back to network.
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            if (response) {
+                console.log('Found ', e.request, ' in cache');
+                return response;
+            }
+            else {
+                console.log('Could not find ', e.request, ' in cache, FETCHING!');
+                return fetch(e.request)
+                .then(function(response) {
+                    const clonedResponse = response.clone();
+                    caches.open('v1').then(function(cache) {
+                        cache.put(e.request, clonedResponse);
+                    })
+                    return response;
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+            }
+        })
+    );
 });
